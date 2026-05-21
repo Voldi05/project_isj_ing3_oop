785f7aef897485cb16041d25d4c4615d271f56ed
Noémie Youbissi <noemieyoubissi@gmail.com>
2026-05-21 22:45:36 +0100

    insertion de l'option de suppression des équipements, d'ajout d'un lien et modification du nom du fichier topologie.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def menu():
     print("5- Quitter")
 
 Continue=True #qui va permettre de gérer l'arret
-table_equip=[] #liste des equipements
+Topo=topologie.Topologie() #définition de notre topologie
 
 while (Continue):
     os.system("cls")
@@ -41,69 +41,78 @@ while (Continue):
             print("5- Terminal")
             print("6- Serveur")
             ch11=int(input("Quel équipement souhaitez-vous ajouter ? (faites un choix situé entre 1 et 4)"))
+            os.system("cls")
             if ch11==1: #ajout d'un switch
                 nom=input("Entrer le nom du switch: ")
-                marque=input("Enter la marque du switch ")
+                marque=input("Enter la marque du switch: ")
                 adresse=input("Entrer l'adresse du switch: ")
                 nb_int=int(input("Entrer le nombre d'interfaces: "))
                 Eq_Switch=equipements.Switch(nom, marque, adresse, nb_int)
-                # equip={"nom": nom, "marque": marque, "adresse": adresse, "nombre interfaces": nb_int, "statut": equipements.Switch.statut}
-                # table_equip.append(equip)
+                Topo.ajouter_equipement(Eq_Switch)
             elif ch11==2: #ajout d'un routeur
                 nom=input("Entrer le nom du routeur: ")
-                marque=input("Enter la marque de l'équipement: ")
-                adresse=input("Entrer l'adresse de l'équipement: ")
+                marque=input("Enter la marque du routeur: ")
+                adresse=input("Entrer l'adresse du routeur: ")
                 nb_int=int(input("Entrer le nombre d'interfaces: "))
                 Eq_Routeur=equipements.Routeur(nom, marque, adresse, nb_int)
-                # equip={"nom": nom, "marque": marque, "adresse": adresse, "nombre interfaces": nb_int, "statut": equipements.Routeur.statut}
-                # table_equip.append(equip)
+                Topo.ajouter_equipement(Eq_Routeur)
+                
             elif ch11==3: #ajout d'un parefeu
                 nom=input("Entrer le nom du parefeu: ")
                 marque=input("Enter la marque du parefeu: ")
                 adresse=input("Entrer l'adresse du parefeu: ")
                 nb_int=int(input("Entrer le nombre d'interfaces: "))
-                Eq_AP=equipements.Firewall(nom, marque, adresse, nb_int)
-                # equip={"nom": nom, "marque": marque, "adresse": adresse, "nombre interfaces": nb_int, "statut": equipements.Firewall.statut}
-                # table_equip.append(equip)
+                Eq_parefeu=equipements.Firewall(nom, marque, adresse, nb_int)
+                Topo.ajouter_equipement(Eq_parefeu)
+                
             elif ch11==4: #ajout d'un point d'accès
                 nom=input("Entrer le nom du point d'accès: ")
                 marque=input("Enter la marque du point d'accès: ")
                 adresse=input("Entrer l'adresse du point d'accès: ")
                 nb_int=int(input("Entrer le nombre d'interfaces: "))
                 ss_id=input("Quel est le ssid du point d'accès: ")
-                Eq_parefeu=equipements.PointAccesWiFi(nom, marque, adresse, nb_int, ss_id)
-                # equip={"nom": nom, "marque": marque, "adresse": adresse, "nombre interfaces": nb_int, "SSID":ss_id ,"statut": equipements.PointAccesWiFi.statut}
-                # table_equip.append(equip)
+                Eq_AP=equipements.PointAccesWiFi(nom, marque, adresse, nb_int, ss_id)
+                Topo.ajouter_equipement(Eq_AP)
 
-            elif ch11==5:
+            elif ch11==5: #ajout d'un terminal
                 nom=input("Entrer le nom du terminal: ")
                 marque=input("Enter la marque du terminal: ")
                 adresse=input("Entrer l'adresse du terminal: ")
                 nb_int=int(input("Entrer le nombre d'interfaces du terminal: "))
                 Eq_Terminal=equipements.TerminalClient(nom, marque, adresse, nb_int)
-                # equip={"nom": nom, "marque": marque, "adresse": adresse, "nombre interfaces": nb_int, "statut": equipements.TerminalClient.statut}
-                # table_equip.append(equip)
+                Topo.ajouter_equipement(Eq_Terminal)
 
-            elif ch11==6:
+            elif ch11==6: #ajour d'un serveur
                 nom=input("Entrer le nom du serveur: ")
                 marque=input("Enter la marque du serveur: ")
                 adresse=input("Entrer l'adresse du serveur: ")
                 nb_int=int(input("Entrer le nombre d'interfaces du serveur: "))
                 Eq_Serveur=equipements.Serveur(nom, marque, adresse, nb_int)
-                # equip={"nom": nom, "marque": marque, "adresse": adresse, "nombre interfaces": nb_int, "statut": equipements.Serveur.statut}
-                # table_equip.append(equip)
-
+                Topo.ajouter_equipement(Eq_Serveur)
             else:
                 print("Choix indisponible !")
-            
-            os.system("pause")
+                os.system("pause")
+                
         elif ch1==2: # ajout d'un lien
-            a=0
-            #je sais pas s'il y aura une fonction d'ajout des liens, mais je vais l'appeler
+            ad_eq1=input("Entrer l'adresse IP du premier équipement: ")
+            ad_eq2=input("Entrer l'adresse IP du deuxième équipement: ")
+            equip1=Topo.trouver_equipement(ad_eq1)
+            equip2=Topo.trouver_equipement(ad_eq2)
+            if equip1==None or equip2==None:
+                print("L'un des équipements que vous avez entré n'existe pas ! Impossible de créer un lien ! ")
+            else:
+                bp=float (input("Entrer la bande passante: "))
+                latence=float (input("Entrer la latence: "))
+                Topo.ajouter_lien(equip1, equip2, bp, latence)
             os.system("pause")
+            
         elif ch1==3: # suppression d'un équipement
-            print("Donner le nom de l'équipement")
-            # suffit juste de supprimer l'équipement dans le dictionnaire des équipeemnts
+            adresse=input("Donner l'adresse IP de l'équipement")
+            equip=Topo.trouver_equipement(adresse)
+            if isinstance(equip):
+                Topo.supprimer_equipement(equip)
+            else:
+                print("Il ne s'agit pas d'un équipement")
             os.system("pause")
         elif ch1==4: #suppression d'un lien
             a=0
